bc28e83cf7a6461e93a05c1d7f71f710690418ca
almeidava93-spesia <almeida.va93@gmail.com>
2025-12-19 01:57:25 -0300

    simplified data storage

--- split_datasets.py
+++ split_datasets.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import jsonlines
 from tqdm import tqdm
 from copy import deepcopy
-from datasets import load_dataset, Dataset
+from datasets import Dataset
 
 random_seed = 42
 
@@ -12,9 +12,6 @@ datasets_base_path = Path("datasets")
 datasets_base_path.mkdir(exist_ok=True)
 datasets_split_path = datasets_base_path / "splits"
 datasets_split_path.mkdir(exist_ok=True)
-datasets_split_msg_format_path = datasets_base_path / "splits-msg-format"
-datasets_split_msg_format_path.mkdir(exist_ok=True)
-
 
 train_data_paths = [
     "datasets/std_concept_to_std_vocab_code.csv",
@@ -36,7 +33,6 @@ for path in train_data_paths:
 
 train_df: pd.DataFrame = pd.concat(train_dfs)
 train_df = train_df.sample(frac=1, random_state=random_seed)
-train_df.to_csv(datasets_split_path / "train.csv", index=False)
 
 # Define val and test data
 for path in val_test_data_paths:
@@ -46,9 +42,7 @@ for path in val_test_data_paths:
 val_test_df: pd.DataFrame = pd.concat(val_test_dfs)
 val_test_df = val_test_df.sample(frac=1, random_state=random_seed)
 val_df = val_test_df.iloc[: int(0.5 * len(val_test_df))]
-val_df.to_csv(datasets_split_path / "val.csv", index=False)
 test_df = val_test_df.iloc[int(0.5 * len(val_test_df)) :]
-test_df.to_csv(datasets_split_path / "test.csv", index=False)
 
 # Save datasets in messages format for LLM instruction tuning
 messages_template = {
@@ -79,9 +73,9 @@ val_dataset = val_dataset.map(format_message)
 test_dataset = test_dataset.map(format_message)
 
 # Save them to disk
-train_dataset.save_to_disk(datasets_split_msg_format_path / "train")
-val_dataset.save_to_disk(datasets_split_msg_format_path / "val")
-test_dataset.save_to_disk(datasets_split_msg_format_path / "test")
+train_dataset.save_to_disk(datasets_split_path / "train")
+val_dataset.save_to_disk(datasets_split_path / "val")
+test_dataset.save_to_disk(datasets_split_path / "test")
 
 print(f"Train data shape: {train_df.shape}")
 print(f"Val data shape: {val_df.shape}")
