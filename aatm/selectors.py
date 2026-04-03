6854ae187d1eda5dda6c2c12f3347942467badce
almeidava93-spesia <vinicius.almeida@spesia.com.br>
2026-04-02 21:16:38 -0300

    added gemini code selector class

--- aatm/selectors.py
+++ aatm/selectors.py
@@ -2,8 +2,10 @@ from enum import Enum
 from typing import Dict, List
 import dotenv
 from abc import ABC, abstractmethod
+from google import genai
+from google.genai import types
 
-from openai import BaseModel, OpenAI
+from openai import OpenAI
 
 # Custom modules
 from aatm.data_models import (
@@ -146,17 +148,115 @@ class OpenAILLMSelector(BaseSelector):
         return selector_results
 
 
+class GeminiLLMModels(Enum):
+    GEMINI_3_PRO_PREVIEW = "gemini-3-pro-preview"
+    GEMINI_3_FLASH_PREVIEW = "gemini-3-flash-preview"
+    GEMINI_2_5_FLASH = "gemini-2.5-flash"
+    GEMINI_2_5_FLASH_LITE = "gemini-2.5-flash-lite"
+    GEMINI_2_5_PRO = "gemini-2.5-pro"
+
+
 class GeminiLLMSelector(BaseSelector):
-    def __init__(self, model_id: str, *args, **kwargs):
-        self.model_id = model_id
+    def __init__(
+        self,
+        model_id: str,
+        prompt_template: List[Dict[str, str]] = None,
+        *args,
+        **kwargs,
+    ):
+        self.model_id = GeminiLLMModels(model_id).value
+        self.client = genai.Client()
+        self.prompt_template = prompt_template
+
+        if self.prompt_template is None:
+            self.prompt_template = [
+                {
+                    "role": "system",
+                    "content": "Select the expression from the list of expressions that is semantically closest to the query. You are mapping a non-standard concept to a standard concept and the list of expressions are already mapped to standard concepts. Your response should be a JSON object and include the expression_id of the selected expression. If no expression matches the query, return '{\"expression_id\": null}'.\n\nJSON object format: {json_format}",
+                },
+                {
+                    "role": "user",
+                    "content": "Query: '{query}'\n\nExpressions: {expressions}",
+                },
+            ]
 
     def select(self, results: RetrieverResults) -> SelectorResults:
-        raise NotImplementedError
+        selector_results = SelectorResults(results=[], queries=results.queries)
+        for query_id, query in enumerate(results.queries):
+            prompt = format_prompt(
+                self.prompt_template,
+                {
+                    "json_format": SelectedResult.model_json_schema(),
+                    "query": query.capitalize(),  # avoid case sensitivity for some queries like 'cough' and 'Cough'
+                    "expressions": [
+                        r.to_prompt_object() for r in results.results[query_id]
+                    ],
+                },
+            )
+
+            gemini_formatted_prompt = []
+            for msg in prompt:
+                gemini_formatted_prompt.append(types.UserContent(msg["content"]))
+
+            if debug_mode == DebugMode.OPENAI_LLM_SELECTOR:
+                logger.debug(prompt)
+
+            response = self.client.models.generate_content(
+                model=self.model_id,
+                contents=gemini_formatted_prompt,
+                config=types.GenerateContentConfig(
+                    response_mime_type="application/json",
+                    response_schema=SelectedResult,
+                ),
+            )
+
+            if debug_mode == DebugMode.GEMINI_LLM_SELECTOR:
+                logger.debug(response)
+                logger.debug(response.text)
+                logger.debug(SelectedResult.model_validate_json(response.text))
+
+            selected_result: SelectedResult = SelectedResult.model_validate_json(
+                response.text
+            )
+            assert isinstance(selected_result, SelectedResult), (
+                f"Expected SelectedResult object from OpenAI, but got {type(selected_result)}"
+            )
+
+            # case where no expression is selected
+            if selected_result.expression_id is None:
+                selector_results.results.append(EmptySelectionMetadata())
+                continue
+
+            # case where expression is selected but it is not in the results
+            results_expression_ids = set(
+                [r.expression_id for r in results.results[query_id]]
+            )
+
+            if selected_result.expression_id not in results_expression_ids:
+                selector_results.results.append(EmptySelectionMetadata())
+                continue
+
+            # case where expression is selected and it is in the results
+            for result_idx, r in enumerate(results.results[query_id]):
+                if r.expression_id == selected_result.expression_id:
+                    selector_results.results.append(
+                        SelectedExpressionMetadata(
+                            **r.model_dump(), result_list_index=result_idx
+                        )
+                    )
+                    break
+
+        return selector_results
 
 
 SELECTOR_REGISTRY = {
     "first": FirstResultSelector,
     "gpt-5.2": OpenAILLMSelector,
+    GeminiLLMModels.GEMINI_3_PRO_PREVIEW.value: GeminiLLMSelector,
+    GeminiLLMModels.GEMINI_3_FLASH_PREVIEW.value: GeminiLLMSelector,
+    GeminiLLMModels.GEMINI_2_5_FLASH.value: GeminiLLMSelector,
+    GeminiLLMModels.GEMINI_2_5_FLASH_LITE.value: GeminiLLMSelector,
+    GeminiLLMModels.GEMINI_2_5_PRO.value: GeminiLLMSelector,
 }
 
 
