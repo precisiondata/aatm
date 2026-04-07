f949b219ebcee79f0ee3cf08ab79bda11d760233
almeidava93-spesia <88211872+almeidava93@users.noreply.github.com>
2026-04-07 00:12:59 -0300

    completed aatm init command
    init command guides the user in setting up the environment locally to perform terminology mapping

--- aatm/main.py
+++ aatm/main.py
@@ -3,15 +3,31 @@ from rich.console import Console
 from pathlib import Path
 import questionary
 from questionary import Choice
+import dotenv
 
-from .local_database_utils import build_local_sqlite_vocab_database
-from .retrievers import CHROMADB_RETRIEVER_MODEL_REGISTRY
+
+from .local_database_utils import (
+    build_local_sqlite_vocab_database,
+    build_local_vector_database,
+    build_mapping_datasets,
+)
+
+dotenv.load_dotenv()  # Load environment variables
 
 app = typer.Typer()
 console = Console()
 
+LOCAL_HELPER_PATH = Path(".aatm")
 DEFAULT_VOCAB_DIR = Path("vocabularies")
 DEFAULT_EMBEDDING_MODEL = "embeddinggemma-300M"
+SUPPORTED_EMBEDDING_MODELS = [
+    "qwen3-06B",
+    "qwen3-4B",
+    "gemini-embedding-001",
+    "embeddinggemma-300M",
+    "text-embedding-3-small",
+    "text-embedding-3-large",
+]
 
 AATM_LOGO = r"""
    █████╗  █████╗ ████████╗███╗   ███╗
@@ -22,9 +38,12 @@ AATM_LOGO = r"""
   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚═╝
 """
 
+
 def print_logo() -> None:
     console.print(f"[bold bright_blue]{AATM_LOGO}[/bold bright_blue]")
-    console.print("[blue]Welcome to the Any-to-Any Terminology Mapping Project![/blue]\n")
+    console.print(
+        "[blue]Welcome to the Any-to-Any Terminology Mapping Project![/blue]\n"
+    )
 
 
 @app.callback(invoke_without_command=True)
@@ -33,10 +52,26 @@ def main(ctx: typer.Context) -> None:
         print_logo()
         console.print("[dim]Use 'aatm --help' to see available commands.[/dim]")
 
+
 @app.command()
 def init(
-    vocab_dir: str = typer.Option(None, "--vocab-dir", "-vd", help="Path to the OMOP vocabularies directory.", show_default=False),
-    ) -> None:
+    vocab_dir: str = typer.Option(
+        None,
+        "--vocab-dir",
+        "-vd",
+        help="Path to the OMOP vocabularies directory.",
+        show_default=False,
+    ),
+) -> None:
+    # Setup local directories
+    LOCAL_HELPER_PATH.mkdir(exist_ok=True, parents=True)
+    gitignore_path = Path(".gitignore")
+    gitignore_path.write_text(
+        gitignore_path.read_text() + "\n.aatm\n"
+        if gitignore_path.exists()
+        else ".aatm\n"
+    )
+
     print_logo()
     console.print("Let's set up your environment!\n")
 
@@ -45,25 +80,48 @@ def init(
     else:
         vocab_dir = Path(vocab_dir)
         if not vocab_dir.exists():
-            console.print(f"[yellow]OOPS![/yellow] You specified a non-existent directory: `{vocab_dir}`. Please, check the path and try again or download the desired OMOP vocabularies at https://athena.ohdsi.org/ if you haven't yet.\n")
+            console.print(
+                f"[yellow]OOPS![/yellow] You specified a non-existent directory: `{vocab_dir}`. Please, check the path and try again or download the desired OMOP vocabularies at https://athena.ohdsi.org/ if you haven't yet.\n"
+            )
             raise typer.Exit()
 
     if not vocab_dir.exists():
-        console.print("[yellow]OOPS![/yellow] You still don't have a `vocabularies` directory. Download the desired OMOP vocabularies at https://athena.ohdsi.org/ and place them in the `./vocabularies` directory or use the `--vocab-dir` option to specify a different directory.\n")
+        console.print(
+            "[yellow]OOPS![/yellow] You still don't have a `vocabularies` directory. Download the desired OMOP vocabularies at https://athena.ohdsi.org/ and place them in the `./vocabularies` directory or use the `--vocab-dir` option to specify a different directory.\n"
+        )
         raise typer.Exit()
 
     console.print("[blue]1) Building local OMOP vocabulary database[/blue]")
     console.print(f"Using vocabulary files from: '{vocab_dir}'")
 
-    #build_local_sqlite_vocab_database(vocab_dir)
+    # build_local_sqlite_vocab_database(vocab_dir)
     console.print("[green]Done![/green]\n")
 
     console.print("[blue]2) Building local vector database[/blue]")
-    supported_embedding_models = list(CHROMADB_RETRIEVER_MODEL_REGISTRY.keys())
+
     supported_embedding_models_choices = [
-        Choice(f"{model_name} (default)", value=model_name) if model_name == DEFAULT_EMBEDDING_MODEL else Choice(model_name, value=model_name) for model_name in supported_embedding_models 
+        Choice(f"{model_name} (default)", value=model_name)
+        if model_name == DEFAULT_EMBEDDING_MODEL
+        else Choice(model_name, value=model_name)
+        for model_name in SUPPORTED_EMBEDDING_MODELS
     ]
     selected_embedding_model = questionary.select(
-    "Select one of the supported embedding models for building the vector database:",
-    choices=supported_embedding_models_choices,
-    default=DEFAULT_EMBEDDING_MODEL).ask()
+        "Select one of the supported embedding models for building the vector database:",
+        choices=supported_embedding_models_choices,
+        default=DEFAULT_EMBEDDING_MODEL,
+    ).ask()
+
+    selected_standard_vocabularies = questionary.checkbox(
+        "Select one or more standard vocabularies to use for mapping:",
+        choices=[
+            Choice("LOINC", value="LOINC", checked=True),
+            Choice("RxNorm", value="RxNorm", checked=True),
+            Choice("SNOMED", value="SNOMED", checked=True),
+        ],
+    ).ask()
+
+    # build_mapping_datasets(selected_standard_vocabularies)
+
+    console.print("Creating local vector database...")
+    build_local_vector_database(selected_embedding_model)
+    console.print("[green]Done![/green]\n")
